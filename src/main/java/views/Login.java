f690cfdae2fafa90b088c4445de1b1cd5d5d4871
ladyangel1981 <51683643+ladyangel1981@users.noreply.github.com>
2021-11-21 21:00:57 +0100

    update login ok

--- src/main/java/views/Login.java
+++ src/main/java/views/Login.java
@@ -3,13 +3,22 @@ package views;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.GraphicsEnvironment;
-import java.awt.FontFormatException;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -24,15 +33,17 @@ import javax.swing.border.EmptyBorder;
 
 import Exception.ErrorControl;
 import Objects.User;
+import Utilities.UserPassObject;
 import XMLconfig.ReadXMLDomParser;
 import modelDAO.UserDao;
-
 
 @SuppressWarnings("serial")
 public class Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField textField;
 	private JPasswordField passwordField;
+	private static String pathUserPass = "src" + File.separator + "main" + File.separator + "java" + File.separator
+			+ "Utilities" + File.separator + "user_pass";
 
 	/**
 	 * Launch the application.
@@ -42,8 +53,7 @@ public class Login extends JFrame {
 			@Override
 			public void run() {
 				try {
-					//HibernateApp apphibernate = new HibernateApp();
-					//apphibernate.main(args);
+					// apphibernate.main(args);
 					ReadXMLDomParser readXMLDomParser = new ReadXMLDomParser();
 					readXMLDomParser.readXML();
 					Login frame = new Login();
@@ -58,11 +68,14 @@ public class Login extends JFrame {
 
 	/**
 	 * Create the frame.
+	 * 
+	 * @throws IOException
 	 */
-	public Login() {
+	public Login() throws IOException {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("KADAMM");
-		ImageIcon img = new ImageIcon("src" + File.separator +"main"+File.separator+"java"+File.separator+ "images" + File.separator + "logoKadamm.PNG");
+		ImageIcon img = new ImageIcon("src" + File.separator + "main" + File.separator + "java" + File.separator
+				+ "images" + File.separator + "logoKadamm.PNG");
 		setIconImage(img.getImage());
 		setResizable(false);
 		setSize(450, 300);
@@ -73,12 +86,14 @@ public class Login extends JFrame {
 		contentPane.setLayout(null);
 
 		JLabel label_1 = new JLabel("");
-		label_1.setIcon(new ImageIcon("src" + File.separator + "main" + File.separator+"java"+File.separator + "images" + File.separator + "movilLoginDegr.png"));
+		label_1.setIcon(new ImageIcon("src" + File.separator + "main" + File.separator + "java" + File.separator
+				+ "images" + File.separator + "movilLoginDegr.png"));
 		label_1.setBounds(240, -35, 235, 350);
 		contentPane.add(label_1);
 
 		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon("src" + File.separator +"main"+File.separator+"java"+File.separator + "images" + File.separator + "trofeoLoginDegr.png"));
+		label.setIcon(new ImageIcon("src" + File.separator + "main" + File.separator + "java" + File.separator
+				+ "images" + File.separator + "trofeoLoginDegr.png"));
 		label.setBounds(-25, -35, 235, 350);
 		contentPane.add(label);
 
@@ -86,14 +101,16 @@ public class Login extends JFrame {
 		lblKadamm.setHorizontalAlignment(SwingConstants.CENTER);
 		lblKadamm.setForeground(Color.CYAN);
 		try {
-		    Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src" + File.separator +"main"+File.separator+"java"+File.separator+"Utilities"+File.separator+"chiller.ttf")).deriveFont(Font.BOLD, 70);
-		    GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		    ge.registerFont(customFont);
-		    lblKadamm.setFont(customFont);
+			Font customFont = Font.createFont(Font.TRUETYPE_FONT, new File("src" + File.separator + "main"
+					+ File.separator + "java" + File.separator + "Utilities" + File.separator + "chiller.ttf"))
+					.deriveFont(Font.BOLD, 70);
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			ge.registerFont(customFont);
+			lblKadamm.setFont(customFont);
 		} catch (IOException e) {
-		    e.printStackTrace();
-		} catch(FontFormatException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
+		} catch (FontFormatException e) {
+			e.printStackTrace();
 		}
 		lblKadamm.setBounds(0, 21, 452, 61);
 		contentPane.add(lblKadamm);
@@ -114,8 +131,30 @@ public class Login extends JFrame {
 		contentPane.add(lblPassword);
 
 		passwordField = new JPasswordField();
+		passwordField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				try {
+					List<UserPassObject> listUser = fileReader();
+					for (UserPassObject x : listUser) {
+						if (x.getUsername().equals(textField.getText())) {
+							passwordField.setText(x.getPassword());
+						}
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
 		passwordField.setBounds(216, 123, 100, 20);
+		System.out.println(passwordField.getAction());
 		contentPane.add(passwordField);
+
+		JCheckBox chckbxRememberMyPassword = new JCheckBox("Remember my password");
+		chckbxRememberMyPassword.setForeground(new Color(175, 238, 238));
+		chckbxRememberMyPassword.setBounds(125, 157, 180, 25);
+		chckbxRememberMyPassword.setBackground(Color.decode("#374151"));
 
 		JButton btnLogin = new JButton("Login");
 		btnLogin.setBounds(170, 192, 110, 35);
@@ -126,47 +165,54 @@ public class Login extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				UserDao userDao = new UserDao();
 				String name = textField.getText();
-				
-				/*
-				User getUser = userDao.getUserByName(name);
-				if (getUser != null) {
-					if (getUser.getPassword().equals(String.valueOf(passwordField.getPassword()))) {
-						KadammManagement kadammManagemente = new KadammManagement();
-						kadammManagemente.main(null);
-					} else {
-						new ErrorControl("Password incorrect", "Warning");
-					}
-				}
-				*/
-				
-				List<User> users = userDao.getAllUsers();
-				//System.out.println(users.size());
-				int contador =0;
-				for (User element : users) {
-					contador++;
-					//System.out.println(element.getUsername());
-					if (name.compareTo(element.getUsername())==0) {
-						if (element.getPassword().equals(String.valueOf(passwordField.getPassword()))) {
+				List<User> userList = userDao.getAllUsers();
+				User user = new User();
+				try {
+					user = userList.stream().filter(x -> x.getUsername().equals(name)).findAny()
+							.orElseThrow(() -> new ErrorControl("User doesn't exist", "Error"));
+					if (user != null) {
+						if (user.getPassword().equals(String.valueOf(passwordField.getPassword()))) {
+							if (chckbxRememberMyPassword.isSelected()) {
+								try {
+
+									File filesavePass = new File(pathUserPass);
+									FileWriter fw = new FileWriter(filesavePass, true);
+									fw.write(textField.getText() + "," + String.valueOf(passwordField.getPassword())
+											+ "\n");
+									fw.close();
+								} catch (IOException e1) {
+									e1.printStackTrace();
+								}
+							}
 							KadammManagement kadammManagemente = new KadammManagement();
 							kadammManagemente.main(null);
-							break;
+							dispose();
 						} else {
 							new ErrorControl("Password incorrect", "Warning");
-							break;
+							passwordField.setText(null);
 						}
 					}
-					if (contador == users.size()){
-						new ErrorControl("No user found!", "Error");
-					}
+
+				} catch (ErrorControl e1) {
+					e1.printStackTrace();
 				}
-				
 			}
 		});
 
-		JCheckBox chckbxRememberMyPassword = new JCheckBox("Remember my password");
-		chckbxRememberMyPassword.setForeground(new Color(175, 238, 238));
-		chckbxRememberMyPassword.setBounds(125, 157, 180, 25);
-		chckbxRememberMyPassword.setBackground(Color.decode("#374151"));
 		contentPane.add(chckbxRememberMyPassword);
+	}
+
+	@SuppressWarnings({ "unused", "rawtypes", "resource" })
+	private static List<UserPassObject> fileReader() throws FileNotFoundException, IOException {
+		FileReader fileReader = new FileReader(new File(pathUserPass));
+		List<UserPassObject> lineaList = new ArrayList<>();
+		BufferedReader br = new BufferedReader(fileReader);
+		String linea = br.readLine();
+		while (linea != null) {
+			List<String> lista = Arrays.stream(linea.split(",")).collect(Collectors.toList());
+			lineaList.add(new UserPassObject(lista.get(0), lista.get(1)));
+			linea = br.readLine();
+		}
+		return lineaList;
 	}
 }
